d272eaa5d836f27ed69cb4d94a8b5d72a0e523e5
Jerry <76718190+JerryLouisDev@users.noreply.github.com>
2022-05-29 22:10:53 -0400

    Create challenges.js

---
+++ challenges.js
@@ -0,0 +1,8 @@
+//Alter the event handler on line 4 to run when the form is submitted, not just when the submit button has been clicked. Leave the callback function empty for now.
+
+const form = document.querySelector('form');
+const submitButton = form.querySelector('[type=Submit]');
+
+form.addEventListener('submit', () => {
+  
+});
